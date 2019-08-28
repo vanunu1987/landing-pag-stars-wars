14c0d293604fcf71b012c82f657733ec9f8abe23
Your Name <vanunu24@gmail.com>
2019-08-28 15:45:54 +0300

    design 2

--- src/components/MoviePreview/MoviePreview.jsx
+++ src/components/MoviePreview/MoviePreview.jsx
@@ -5,11 +5,12 @@ const MoviePreview = (props) =>{
     let movieTitle = props.movie.title
     let movie = props.savedMovie && Object.keys(props.savedMovie).find(movie=> movie===movieTitle)
     let fab = (props.savedMovie[movie])?"fas fa-bookmark ":"far fa-bookmark "
-    
+    let img = utilService.getImg(movieTitle)
     
     
    return (
        <div className={classes['preview-container']}>
+       <div className={classes['img-movie']} style={{backgroundImage: `url(${img})`}}></div>
        <h1>{movieTitle}</h1>
         <span onClick={()=>{props.click(movieTitle)}} className={fab}></span>
        </div>
--- src/services/utilService.js
+++ src/services/utilService.js
@@ -8,8 +8,23 @@ function loadFromStorage(key) {
     return JSON.parse(str)
 }
 
+var imges = {
+    "Attack of the Clones": "https://vignette.wikia.nocookie.net/starwars/images/9/98/Aotctpb.jpg/revision/latest?cb=20110206033257",
+    "A New Hope": "https://starwarsblog.starwars.com/wp-content/uploads/2014/05/a-new-hope.jpg",
+    "The Phantom Menace": "https://m.media-amazon.com/images/M/MV5BYTRhNjcwNWQtMGJmMi00NmQyLWE2YzItODVmMTdjNWI0ZDA2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg",
+    "Revenge of the Sith": "https://images-na.ssl-images-amazon.com/images/I/81cUuRhZw%2BL._SL1500_.jpg",
+    "Return of the Jedi": "https://images-na.ssl-images-amazon.com/images/I/81g8vEs4ixL.jpg",
+    "The Empire Strikes Back":"https://images-na.ssl-images-amazon.com/images/I/91eOgodm4nL.jpg",
+    "The Force Awakens": "https://images-na.ssl-images-amazon.com/images/I/91xZGOnCFSL._SL1500_.jpg"
+}
+
+function getImg(title){
+return imges[title]
+}
+
 export default {
     saveToStorage,
-    loadFromStorage
+    loadFromStorage,
+    getImg
 
 }
