3b27f92fbde2eae475bff8f31c8e0b058bf67fa4
Your Name <vanunu24@gmail.com>
2019-08-30 12:20:08 +0300

    Updates

---
+++ precache-manifest.baf507381534ba25ecdfa37993065ced.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17345f5d71ac4439eb6928c2c753acff",
+    "url": "/landing-pag-stars-war/index.html"
+  },
+  {
+    "revision": "7c26608353c0fa88a4af",
+    "url": "/landing-pag-stars-war/static/css/main.3d33d563.chunk.css"
+  },
+  {
+    "revision": "a693d6ee5977c1c2aaf8",
+    "url": "/landing-pag-stars-war/static/js/2.3802c709.chunk.js"
+  },
+  {
+    "revision": "7c26608353c0fa88a4af",
+    "url": "/landing-pag-stars-war/static/js/main.7d9f2266.chunk.js"
+  },
+  {
+    "revision": "2d9e0329f41756987f05",
+    "url": "/landing-pag-stars-war/static/js/runtime~main.75226b79.js"
+  },
+  {
+    "revision": "f72f509a2014f09c1ebf1c81294ed792",
+    "url": "/landing-pag-stars-war/static/media/Starjout.f72f509a.ttf"
+  },
+  {
+    "revision": "65cf4d05ca1253625bedd8b556e47838",
+    "url": "/landing-pag-stars-war/static/media/background.65cf4d05.png"
+  },
+  {
+    "revision": "c19759a5042ae33a1ceb3a87efed741c",
+    "url": "/landing-pag-stars-war/static/media/header.c19759a5.jpg"
+  }
+]);
